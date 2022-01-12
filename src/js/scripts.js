5a6a5d01a84e118d11de86526782b915be973b81
Dan Pierce <danpierce12@gmail.com>
2022-01-12 23:26:15 +0000

    Misc HTML & CSS tweaks

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,12 @@
 ﻿if (document.readyState !== "loading") {
   toggleNav();
+  toggleModal();
   toggleAccordion();
   initCarousel();
 } else {
   document.addEventListener("DOMContentLoaded", function () {
     toggleNav();
+    toggleModal();
     toggleAccordion();
     initCarousel();
   });
@@ -14,6 +16,7 @@
 function toggleNav() {
   const openNavBtn = document.querySelector(".js-open-main-nav");
   const closeNavBtn = document.querySelector(".js-close-main-nav");
+  const navItem = document.querySelectorAll(".js-nav-item");
 
   openNavBtn.addEventListener("click", function (e) {
     e.preventDefault();
@@ -26,6 +29,33 @@ function toggleNav() {
     document.querySelector(".main-nav").classList.remove("open");
     document.body.classList.remove("noscroll");
   });
+
+  navItem.forEach(function (e) {
+    e.addEventListener("click", function () {
+      document.querySelector(".main-nav").classList.remove("open");
+      document.body.classList.remove("noscroll");
+    });
+  });
+}
+
+// toggle modal overlay
+function toggleModal() {
+  const openModalBtn = document.querySelector(".js-open-modal");
+  const closeModalBtn = document.querySelector(".js-close-modal");
+
+  if (document.body.contains(openModalBtn)) {
+    openModalBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(".modal").classList.add("open");
+      document.body.classList.add("noscroll");
+    });
+
+    closeModalBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(".modal").classList.remove("open");
+      document.body.classList.remove("noscroll");
+    });
+  }
 }
 
 // show/hide accordion content
@@ -35,7 +65,7 @@ function toggleAccordion() {
   if (document.body.contains(document.querySelector(".js-accordion-toggle"))) {
     accordion.forEach(function (e) {
       e.addEventListener("click", function () {
-        this.parentNode.classList.toggle("show");
+        this.parentNode.classList.toggle("open");
       });
     });
   }
